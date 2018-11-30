d60a2cffafa365b39a80e3721e101e8742a8269d
kilodelta8 <kilo.kilo.delta8@gmail.com>
2018-11-29 20:28:36 -0500

    First Commit

---
+++ lottogen.cpp
@@ -0,0 +1,10 @@
+/*=====================================================================================
+@                                     LottoGen v1
+@"LottoGen" is STRICTLY free and for entertainment purposes ONLY!  There is absolutely
+@no guarantee and no one is responsible for anything whatsoever that may or may not
+@come of using this code.  Compile and dream at your own risk.
+@--------------------------------------------------------------------------------------
+@LottoGen is a program that calculates "winning numbers" for whichever lottery data
+@that YOU provide via a text file. 
+@
+@======================================================================================*/
